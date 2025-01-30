13a2aa6610a6338539ccbc2854ec87dbd054d06c
Alvar Quezada <alquezada._@hotmail.com>
2025-01-30 11:40:26 -0600

    commit 16
    Creando el Endpoint para obtener todos los productos

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,11 +1,24 @@
 import { NextFunction, Request, Response } from 'express'
 import Product from '../models/Product.model'
 
+export const getProducts = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const products = await Product.findAll({
+            order: [
+                ['price', 'DESC']
+            ]
+        })
+        res.json({ data: products })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 export const createProduct = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         const product = await Product.create(req.body)
         res.json({ data: product })
-    } catch (error) { 
+    } catch (error) {
         console.error(error)
     }
 }
