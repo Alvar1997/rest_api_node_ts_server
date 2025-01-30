d0c86b8bb4b223bff4fa38661dfdcdb57edd4b6e
Alvar Quezada <alquezada._@hotmail.com>
2025-01-30 11:05:59 -0600

    commit 14
    Validando crear producto en el servidor

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,7 +1,26 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import { check, validationResult } from 'express-validator'
 import Product from '../models/Product.model'
 
-export const createProduct = async (req: Request, res: Response) => {
+
+export const createProduct = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+
+    //Validación
+    await check('name')
+        .notEmpty().withMessage('El nombre del producto no puede ir vacio.')
+        .run(req)
+
+    await check('price')
+        .isNumeric().withMessage('Valor no válido.')
+        .notEmpty().withMessage('El precio del Producto no puede ir vacio.')
+        .custom(value => value > 0).withMessage('Precio no válido.')
+        .run(req)
+
+    let errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() })
+        return
+    }
 
     const product = await Product.create(req.body)
     res.json({ data: product })
