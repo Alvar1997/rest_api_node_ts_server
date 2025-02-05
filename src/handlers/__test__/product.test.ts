0ca8649b0c4136ff147b93a7eefba7ed2f35ddf4
Alvar Quezada <alquezada._@hotmail.com>
2025-02-04 18:26:06 -0600

    commit 27
    Probando la url de productos

--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -51,5 +51,20 @@ describe('POST /api/products', () => {
         expect(response.status).not.toBe(200)
         expect(response.body).not.toHaveProperty('errors')
     })
+})
 
+describe('GET /api/products', () => {
+    it('should check if api/products URL exists', async () => {
+        const response = await request(server).get('/api/products')
+        expect(response.status).not.toBe(404)
+    })
+    it('GET a JSON response with products', async () => {
+        const response = await request(server).get('/api/products')
+        expect(response.status).toEqual(200)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body).toHaveProperty('data')
+        expect(response.body.data).toHaveLength(1)
+        expect(response.status).not.toBe(404)
+        expect(response.body).not.toHaveProperty('errors')
+    })
 })
