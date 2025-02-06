522a79456785cff3ca350e17c325138e91f86c7d
Alvar Quezada <alquezada._@hotmail.com>
2025-02-06 11:08:20 -0600

    commit 31
    Metricas de validación - Code Coverage

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,42 +2,30 @@ import { NextFunction, Request, Response } from 'express'
 import Product from '../models/Product.model'
 
 export const getProducts = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const products = await Product.findAll({
-            order: [
-                ['price', 'DESC']
-            ]
-        })
-        res.json({ data: products })
-    } catch (error) {
-        console.error(error)
-    }
+    const products = await Product.findAll({
+        order: [
+            ['price', 'DESC']
+        ]
+    })
+    res.json({ data: products })
 }
 
 export const getProductById = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params
-        const product = await Product.findByPk(id)
+    const { id } = req.params
+    const product = await Product.findByPk(id)
 
-        if (!product) {
-            res.status(404).json({ error: 'Product not found' })
-            return
-        }
-
-        res.json({ data: product })
-    } catch (error) {
-        console.error(error)
+    if (!product) {
+        res.status(404).json({ error: 'Product not found' })
+        return
     }
+
+    res.json({ data: product })
 }
 
 
 export const createProduct = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(201).json({ data: product })
-    } catch (error) {
-        console.error(error)
-    }
+    const product = await Product.create(req.body)
+    res.status(201).json({ data: product })
 }
 
 export const updateProduct = async (req: Request, res: Response): Promise<void> => {
