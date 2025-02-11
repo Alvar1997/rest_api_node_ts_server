92c4307e985c11876c9a47d9149aff80f7efde20
Alvar Quezada <alquezada._@hotmail.com>
2025-02-11 12:14:40 -0600

    commit 35
    Documentando todos los Productos

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,8 +1,8 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
-const options: swaggerJSDoc.Options = {
+const options : swaggerJSDoc.Options = {
     swaggerDefinition: {
-        opneapi: "3.0.0",
+        openapi: "3.0.0",
         tags: [
             {
                 name: 'Products',
